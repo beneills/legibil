a681dbb497418eb4452b5853c131b8bf3ead1553
Ben Eills <ben@beneills.com>
2016-02-03 15:41:39 +0000

    add devise for user authentification; basic setup

--- frontend/test/models/user_test.rb
+++ frontend/test/models/user_test.rb
@@ -2,10 +2,33 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
+  # negative tests
+
+  test "should not save user without email" do
+    user = User.new
+    user.password = "password"
+    assert_not user.save, "Couldn't save the user with good data"
+  end
+
+  test "should not save user without password" do
+    user = User.new
+    user.email    = "user1@example.com"
+    assert_not user.save, "Couldn't save the user with good data"
+  end
+
+  test "should not save user with short password" do
+    user = User.new
+    user.email    = "user1@example.com"
+    user.password = "a" * 7
+    assert_not user.save, "Couldn't save the user with good data"
+  end
+
   # positive tests
 
   test "should save user with good data" do
     user = User.new
+    user.email    = "user1@example.com"
+    user.password = "password"
     assert user.save, "Couldn't save the user with good data"
   end
 end
