62cf1197e9fc032d638cb44a0e529d54f7f7a5c3
Ben Eills <ben@beneills.com>
2016-02-06 21:55:25 +0000

    Refactor manual refresh job

--- app/jobs/manually_refresh_endpoint_job.rb
+++ app/jobs/manually_refresh_endpoint_job.rb
@@ -1,4 +1,5 @@
 require 'mini_magick'
+require 'tempfile'
 
 class ManuallyRefreshEndpointJob < ActiveJob::Base
   queue_as :urgent
@@ -7,56 +8,92 @@ class ManuallyRefreshEndpointJob < ActiveJob::Base
     logger.debug "refreshing endpoint #{endpoint.url} by user #{endpoint.user.email}"
 
     # grab page screenshot
+    screenshot = grab_page_screenshot(endpoint.url)
+
+    # render focus area and attach to endpoint
+    save_focus_area(endpoint, screenshot)
+
+    # update refresh time
+    endpoint.last_refreshed_at = Time.now
+    endpoint.save!
+  end
+
+  private
+
+  def grab_page_screenshot(url)
+    screenshot = new_focus_view_image_filename
+
     driver = Selenium::WebDriver.for :firefox
-    driver.get endpoint.url
-    driver.save_screenshot('/tmp/page.png')
+    driver.get url
+    driver.save_screenshot(screenshot)
     driver.quit
 
-    # render background
-    image = MiniMagick::Image.open('/tmp/page.png')
+    screenshot
+  end
+
+  def new_focus_view_image_filename
+    Dir::Tmpname.make_tmpname([Rails.root.join('tmp/focus-view-').to_s, '.png'], nil)
+  end
+
+  def render_blurry_background(screenshot)
+    background = new_focus_view_image_filename
+
+    image = MiniMagick::Image.open(screenshot)
     image.combine_options do |b|
       b.colorspace 'Gray'
       b.blur       '0x6'
     end
     image.format 'png'
-    image.write  '/tmp/page_background.png'
+    image.write  background
 
-    # render focus area
-    image = MiniMagick::Image.open('/tmp/page.png')
+    background
+  end
+
+  def render_focus_centre(screenshot)
+    centre = new_focus_view_image_filename
+
+    image = MiniMagick::Image.open(screenshot)
     image.combine_options do |b|
-      b.gravity 'Center'
-      b.crop    '30x30%+0x0'
+      b.gravity     'Center'
+      b.crop        '30x30%+0x0'
       b.bordercolor 'Red'
-      b.border       '1x1'
+      b.border      '1x1'
     end
-    image.format     'png'
-    image.write      '/tmp/page_center.png'
+    image.format 'png'
+    image.write  centre
 
-    # join images together
-    background = MiniMagick::Image.new('/tmp/page_background.png')
-    focus      = MiniMagick::Image.new('/tmp/page_center.png')
-    result = background.composite(focus) do |c|
+    centre
+  end
+
+  def merge_background_and_centre(background, centre)
+    merged = new_focus_view_image_filename
+
+    logger.debug "background #{background}"
+    logger.debug "centre #{centre}"
+
+    background = MiniMagick::Image.new(background)
+    centre     = MiniMagick::Image.new(centre)
+    result = background.composite(centre) do |c|
       c.gravity  'Center'
       c.geometry '+0+0'
     end
-    result.write '/tmp/page_focus_view.png'
+    result.format 'png'
+    result.write  merged
 
-    # save focus view screenshot
-    File.open('/tmp/page_focus_view.png') do |f|
+    merged
+  end
+
+  def save_focus_area(endpoint, screenshot)
+    background = render_blurry_background(screenshot)
+    centre     = render_focus_centre(screenshot)
+    focus_area = merge_background_and_centre(background, centre)
+
+    File.open(focus_area) do |f|
       # TODO does this successfully update?
       focus_view = endpoint.build_focus_view
 
       focus_view.screenshot = f
-
-      unless focus_view.save
-        logger.error "could not save focus view/screenshot for endpoint #{endpoint.id}"
-      end
-    end
-
-    # update refresh time
-    endpoint.last_refreshed_at = Time.now
-    unless endpoint.save
-      logger.error "could not save updated refresh time for endpoint #{endpoint.id}"
+      focus_view.save!
     end
   end
 end
