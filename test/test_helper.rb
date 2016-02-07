6a9c70b989bef8315acfc2d4a1ac6a2dbee3fa84
Ben Eills <ben@beneills.com>
2016-02-07 12:47:10 +0000

    Ensure rails tmp directory exists before tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,8 +6,17 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  setup :ensure_rails_tmp_exists
+
   def refreshed_recently?(endpoint)
     endpoint.ever_refreshed? and (Time.now - endpoint.last_refreshed_at).abs < 1
+  end
+
+  private
+
+  def ensure_rails_tmp_exists
+    rails_tmp = Rails.root.join('tmp')
+    Dir.mkdir(rails_tmp) unless File.exists?(rails_tmp)
   end
 end
 
