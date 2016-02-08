8433f8f628eac588e21577ffe169a99204793fe9
Ben Eills <ben@beneills.com>
2016-02-08 16:16:06 +0000

    Allow deployment of other branches

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'UX'
 set :repo_url, 'https://github.com/beneills/ux.git'
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/srv/ux.beneills.com'
